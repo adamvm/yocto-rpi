7579e08c1a5e3f6631dd76776dc576940e0106e3
Adam <5563693+adamvm@users.noreply.github.com>
2022-04-18 13:10:03 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt install git -y
+
+sudo apt install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+     build-essential chrpath socat libsdl1.2-dev xterm -y
+     
+git clone -b dunfell git://git.yoctoproject.org/poky.git && cd poky
+
+git clone -b dunfell git://git.openembedded.org/meta-openembedded && git clone -b dunfell git://git.yoctoproject.org/meta-raspberrypi
+
+source oe-init-build-env build-rpi
+
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+
+bitbake-layers add-layer ../meta-openembedded/meta-python
+
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+
+bitbake-layers add-layer ../meta-raspberrypi
+
+echo 'MACHINE = "raspberrypi3-64"' >> conf/local.conf
+
+echo 'EXTRA_IMAGE_FEATURES ?= "debug-tweaks ssh-server-openssh"' >> conf/local.conf
+
+bitbake rpi-test-image --runall=fetch && bitbake rpi-test-image
